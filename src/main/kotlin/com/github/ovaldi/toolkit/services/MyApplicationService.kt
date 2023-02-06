47e1a44a062546684d0d96376a0f4b095e87b871
GitHub Action <action@github.com>
2023-02-06 14:27:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/ovaldi/toolkit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ovaldi/toolkit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ovaldi.toolkit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ovaldi.toolkit.MyBundle
 
 class MyApplicationService {
 
